14b13fa6642e6a7430bed2454bba2ce64254c813
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-08-21 17:11:54 +0100

    Add files via upload

--- Self_attention_mechanism/Python_model/Feed_forward.py
+++ Self_attention_mechanism/Python_model/Feed_forward.py
@@ -7,8 +7,10 @@ class FeedForward(nn.Model):
   def __init__(self, n_embd):
     super().__init__()
     self.net = nn.Sequential(
-      nn.Linear(n_embd, n_embd),
+      nn.Linear(n_embd, 4 *  n_embd),
       nn.ReLU(),
+      # This is the projection layer going back to residual path-way
+      nn.Linear( 4 * n_embd, n_embd),
     )
     
   def forward(self, x):
--- Self_attention_mechanism/Python_model/Multi_head_attention.py
+++ Self_attention_mechanism/Python_model/Multi_head_attention.py
@@ -57,12 +57,13 @@ class MultiHeadAttention(nn.Module):
   def __init__(self, num_heads, head_size):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-    self.proj = nn.Linear(head_size * num_heads, n_embd)
-    self.dropout = nn.Dropout(dropout)
+    # Here we introduce projection
+    self.proj = nn.Linear(n_embd, n_embd)
     
   def __forward__(self, x):
     out = torch.cat([h(x) for h in self.heads], dim=-1)
-    out = self.dropout(self.proj(out))
+    # This is projection back to residual path-way
+    out = self.proj(out)
     return out
   
   
